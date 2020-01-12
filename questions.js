d01b82065c48aa80a469c8316f9858f92c8e4874
Kelly Bazella <kelly.bazella@gmail.com>
2020-01-11 20:13:36 -0600

    created HTML CSS and JS files, added questions

---
+++ questions.js
@@ -0,0 +1,33 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+        title: "Inside which HTML element do we put JavaScript?",
+        choices: ["<script>", "<js>", "<javascript>","scripting"],
+        answer: "script"
+    },
+    {
+        title:"What is the correct syntax for referring to an external script called xxx.js?",
+        choices:["<script href='xxx.js'>", "<script name='xxx.js'>","<script src='xxx.js'"],
+        answer: "<script src='xxx.js'>"
+    },
+    {
+        title:"How would you write 'Hello World' in an alert box?",
+        choices:["alert('Hello World');", "alertBox('Hello World');","msg('Hello World');"],
+        answer: "alert('Hello World');"
+    },
+    {
+        title:"How do you create a function in JavaScript?",
+        choices:["function myFunction()", "function= myFunction","function:myFuncation()"],
+        answer: "function myFunction()"
+    }
+  ];
+  
